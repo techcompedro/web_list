adc5ba32b9abae9c83c9ee38050250ed9bfa53ff
Pedro Henrique <Pedrohenrique16870@gmail.com>
2024-08-16 14:48:12 -0300

    eu apaguei o codigo antigo e fiz tudo zero esse o web list 2.0

--- app.py
+++ app.py
@@ -1,33 +1,72 @@
 from flask import Flask, render_template, request, redirect
-from database import Lista, session
-
+from database import Lista, session, func, extract
+import datetime as dt 
 app = Flask(__name__)
 
-@app.route('/')
-def lista():
-    lista = session.query(Lista).all()
-    return render_template('index.html', lista=lista)
 
-@app.route('/salvar_lista', methods=['POST'])
-def salvar_lista():
+@app.route('/')
+def index ():
+    if request.method == 'POST':
+        data_str = request.form.get('data')  # Recebe a data do formulário como string
+        data_obj = dt.strptime(data_str, '%Y-%m-%d').date()
+    else:
+        data_obj = None  # Defina um valor padrão se a data não for enviada
+
+        # Total de tarefas
+    all_items = session.query(Lista).all()
+    total = len(all_items)
+
+    # Tarefas pendentes e feitas
+    contagem_feita_true = session.query(func.count(Lista.feita)).filter(Lista.feita == True).scalar()
+    contagem_feita_false = session.query(func.count(Lista.feita)).filter(Lista.feita == False).scalar()
+
+    return render_template('index.html', lista=all_items, total=total, feita_false=contagem_feita_false, feita_true=contagem_feita_true)
+
+
+
+@app.route('/exibir_lista', methods=['GET', 'POST'])
+def exibir_listaa():
+    listas_hoje = session.query(Lista).filter(
+        extract('day', Lista.data) == dt.date.today().day,
+        extract('month', Lista.data) == dt.date.today().month,
+        extract('year', Lista.data) == dt.date.today().year
+    ).all()
+    
+    if request.method == 'POST':
+        lista_selecionada = request.form.get('lista')  # Pegando o valor do dropdown
+        
+        if lista_selecionada == 'lista_hoje':
+            lista = listas_hoje
+        elif lista_selecionada == 'lista_mes':
+            lista = session.query(Lista).filter(
+                extract('month', Lista.data) == dt.date.today().month,
+                extract('year', Lista.data) == dt.date.today().year
+            ).all()
+        elif lista_selecionada == 'lista_ano':
+            lista = session.query(Lista).filter(
+                extract('year', Lista.data) == dt.date.today().year
+            ).all()
+        else:
+            lista = []
+
+        return render_template('index.html', lista=lista)
+
+    
+
+@app.route('/adicionar_lista', methods=['POST'])
+def addlista():
+    data_str = request.form['data']
+    data_obj = dt.strptime(data_str, '%Y-%m-%d').date()
     nova_lista = Lista(
-        lista = request.form['lista']
+        lista=request.form['lista'],
+        descricao=request.form['descricao'],
+        data=data_obj
     )
     session.add(nova_lista)
     session.commit()
     return redirect('/')
 
-
-
-
-@app.route('/editar_lista',  methods=['POST'])
-def editar():
-    lista_id  = request.form['id']
-    list = session.query(Lista).get(lista_id)
-    if list:
-        list.lista = request.form['lista']
-        session.commit()
-    return redirect('/')
+    
 
 @app.route('/delete', methods=['POST'])
 def delete():
@@ -37,7 +76,7 @@ def delete():
         session.delete(lista_id)
         session.commit()
     return redirect('/')
-    
+
 @app.route('/check', methods=['POST'])
 def check():
     lista_id = request.form['id']
@@ -48,18 +87,6 @@ def check():
    
     session.rollback()
     return redirect('/')
-
-    
-        
-
-
-
-
-
-
-
-        
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, func, Date, extract
+from sqlalchemy.orm import declarative_base, sessionmaker 
+from datetime import datetime 
 
 engine = create_engine('sqlite:///lista.db', echo=True)
 
@@ -9,15 +10,11 @@ class Lista(Base):
     __tablename__ = 'lista'
     id = Column(Integer, primary_key=True)
     lista = Column(String(150), nullable=False)
+    descricao = Column(String(300), nullable=True)
     feita = Column(Boolean,  default=False)
+    data = Column(Date, nullable=True)
     
-    
+
 Session = sessionmaker(bind=engine)
-session = Session()    
-
-#Base.metadata.create_all(engine)
-
-
-
-    
-    
+session = Session()
+Base.metadata.create_all(engine)    
